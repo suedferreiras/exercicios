c66139eda46737c17e206d483f0ba9e8f37eff9e
Sued Ellen <suedwor@gmail.com>
2026-08-03 19:55:38 +0000

    feita parcialmente, falta repetição

--- calculadorasimples.c
+++ calculadorasimples.c
@@ -3,20 +3,37 @@
 int main(){
 
     float a, b, m, d, s, r;
-    char o;
+    char o; 
     
     printf("digite a operação usando um dos operadores a seguir: - + * /\n");
     scanf("%f  %c %f", &a, &o, &b );
     printf("Você digitou: %.2f %c %.2f\n", a, o, b);
 
-    m = a * b; 
+    m = a * b; d = a / b; s = a + b;r = a - b;
+
     switch (o)
     {
     case '*':
-        printf(" %f  %c %f = %f\n", a, o, b, m);
+        printf("%.2f  %c %.2f = %.2f\n", a, o, b, m);
         break;
-    
+    case '/':
+        if (a / b && b != 0){
+            printf("%.2f  %c %.2f = %.2f\n", a, o, b, d);
+        } else {
+            printf("A divisao nao pode ser por 0.\n");
+        }
+        break;
+    case '+':
+        printf("%.2f  %c %.2f = %.2f\n", a, o, b, s);
+        break;
+    case '-':    
+        printf("%.2f  %c %.2f = %.2f\n", a, o, b, r);
+        break;
     default:
+        if( o != '*' && o != '/' && o != '+' && o != '-' ){
+            printf("Erro, digite novamente uma operação entre apenas dois numeros: \n");
+        }
+
         break;
     }
 
