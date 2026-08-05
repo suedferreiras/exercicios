d36a8b98adb4682a597c5ccd32c307edfc6d57dc
Sued Ellen <suedwor@gmail.com>
2026-08-05 23:15:45 +0000

    revisado

--- produtos.mercado.c
+++ produtos.mercado.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    char produtos[5][20]; int quantidade[5], estoque[3], n; float preco[5];
+    char produtos[5][20]; int quantidade[5], n; float preco[5], estoque[5];
 
     for(n = 1; n <= 5; n++){
         printf("nome do %dº produto: \n", n); 
@@ -11,15 +11,15 @@ int main(){
         printf("preço: \n"); 
         scanf("%f", &preco[n]);
     }
+    printf("\n *****RELATÓRIO*****\n");
     for(n = 1; n <= 5; n++){
         estoque[n] = preco[n] * quantidade[n];
-        printf("\n *****RELATÓRIO*****\n");
         if(estoque[n] < 10){
-            printf(" PRODUTO: %s - QUANTIDADE: %d - PREÇO: %.2f - ESTOQUE BAIXO \n", produtos[n], quantidade[n], preco[n]);
+            printf(" \nPRODUTO: %s - QUANTIDADE: %d - PREÇO: %.2f - VALOR TOTAL ESTOQUE: %.2f - ESTOQUE BAIXO \n", produtos[n], quantidade[n], preco[n], estoque[n]);
         } else if (estoque[n] <= 50){
-            printf(" PRODUTO: %s - QUANTIDADE: %d - PREÇO: %.2f - ESTOQUE MÉDIO \n", produtos[n], quantidade[n], preco[n]);
+            printf(" \nPRODUTO: %s - QUANTIDADE: %d - PREÇO: %.2f - VALOR TOTAL ESTOQUE: %.2f - ESTOQUE MÉDIO \n", produtos[n], quantidade[n], preco[n], estoque[n]);
         } else {
-            printf(" PRODUTO: %s - QUANTIDADE: %d - PREÇO: %.2f - ESTOQUE OK \n", produtos[n], quantidade[n], preco[n]);
+            printf(" \nPRODUTO: %s - QUANTIDADE: %d - PREÇO: %.2f - VALOR TOTAL ESTOQUE: %.2f - ESTOQUE OK \n", produtos[n], quantidade[n], preco[n], estoque[n]);
         }
     }
 
